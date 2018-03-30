5d772dd22218bd4c7b92dd184859294417653d18
Sergei Ogarkov <sogarko1@gmail.com>
2018-03-30 21:59:51 +0300

    HW 5 env preparation 2

--- public/getpage.php
+++ public/getpage.php
@@ -5,7 +5,7 @@ header("Cache-Control: no-store, no-cache, must-revalidate");
 header("Expires: " . date("r"));
 include_once $_SERVER['DOCUMENT_ROOT'] . "/../config/main.php";
 require_once ENGINE_DIR . "/render.php";
-require_once TEMPLATES_DIR . "/template.php";
+require_once ENGINE_DIR . "/template.php";
 
 $page_no = $_GET['page_no'];
 displayGallerySection($parse, $page_no);
